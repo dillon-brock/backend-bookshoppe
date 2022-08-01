3c7325e95ba2c0d5d9720d7fcca8eed31be8a5e6
Dillon Brock <mdillon1297@gmail.com>
2022-08-01 16:22:49 -0700

    working author list route, passing test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('books routes', () => {
+describe('routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
@@ -13,15 +13,6 @@ describe('books routes', () => {
     const blackHoles = res.body.find((book) => book.title === 'The Little Book of Black Holes');
     expect(blackHoles).toHaveProperty('released', 2017);
   });
-  afterAll(() => {
-    pool.end();
-  });
-});
-
-describe ('author routes', () => {
-  beforeEach(() => {
-    return setup(pool);
-  });
   it('should return a list of authors with id and name', async () => {
     const res = await request(app).get('/authors');
     expect(res.body.length).toEqual(7);
@@ -29,5 +20,8 @@ describe ('author routes', () => {
       id: expect.any(String),
       name: expect.any(String)
     });
+  });
+  afterAll(() => {
+    pool.end();
   });
 });
